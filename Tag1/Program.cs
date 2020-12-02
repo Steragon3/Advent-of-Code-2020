6e539635f10f186015e780c61ef6c54eb6bb8b60
Stefan Maier <smaier.mmt-b2019@fh-salzburg.ac.at>
2020-12-02 09:25:54 +0100

    Tag1 & Tag2

---
+++ Tag1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tag1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader("input.txt");
+            var lines = new List<int>();
+            string line;
+            int n;
+            while ((line = reader.ReadLine()) != null)
+            {
+                n = int.Parse(line);
+                lines.Add(n);
+            }
+
+            foreach(var a in lines){
+                foreach(var b in lines.Where(e => e != a)){
+
+                    foreach(var c in lines.Where(e => e != a && e != b)){
+
+                        if((a+b+c) == 2020){
+                            Console.WriteLine($"a {a}, b {b}, {c}");
+                            Console.WriteLine(a*b*c);
+                        } 
+                    }
+                }
+            }
+
+            reader.Close();
+
+        }
+    }
+}
---
+++ Tag2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tag2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader("input.txt");
+            var passwords = new List<Password>();
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                try{  
+                    passwords.Add(new Password(line.Split(" ")));
+                }catch(Exception e){
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine(passwords.Count());
+            Console.WriteLine(passwords.Where(e => e.IsValid()).Count());
+
+            reader.Close();
+        }
+
+    }
+
+    class Password
+    {
+        public int Min, Max;
+        public char Character;
+        public string PasswordValue;
+
+        public Password(int min, int max, char character, string password)
+        {
+            Min = min;
+            Max = max;
+            Character = character;
+            PasswordValue = password;
+        }
+
+        public Password(string[] parts){
+            try{
+                if(parts[1].Trim(':').Length != 1){
+                    throw new InvalidDataException();
+                }
+
+                Min = int.Parse(parts[0].Split('-')[0]);
+                Max = int.Parse(parts[0].Split('-')[1]);
+                Character = parts[1].Trim(':')[0];
+                PasswordValue = parts[2];
+            } catch(Exception e){
+                throw e;
+            }
+            
+        }
+
+        public bool IsValid(){
+            try{
+                var first = PasswordValue[Min-1] == Character;
+                var second = PasswordValue[Max-1] == Character;
+                return first ^ second; 
+            }catch{
+                return false;
+            }
+
+        }
+    }
+
+}
